022e3c6e46c9393ba498c529cc75841e56366f5f
peterporte <ernestopedroeport@gmail.com>
2021-01-05 02:00:15 +0800

    TDD practice

--- SampleFramework1/SampleApplicationOneTests.cs
+++ SampleFramework1/SampleApplicationOneTests.cs
@@ -12,21 +12,68 @@ namespace SampleFramework1
     public class SampleApplicationOneTests
     {
         private IWebDriver Driver { get; set; }
+        internal SampleApplicationPage SampleAppPage { get; private set; }        
+        internal TestUser TheTestUser { get;  private set; }
+        internal TestUser EmergencyContactUser { get; private set; }
+
+        [TestMethod]
+        [Description("Validate that user is able to fill out the form successfully using valid data.")]
+        public void Test1()
+        {
+            SetGenderTypes(Gender.Female, Gender.Female);
+
+            //var sampleApplicationPage = new SampleApplicationPage(Driver);
+            //sampleApplicationPage.GoTo();
+            SampleAppPage.GoTo();
+            SampleAppPage.FilloutEmergencyContact(EmergencyContactUser);
+            var ultimateQAHomePage = SampleAppPage.FillOutPrimaryContactFormAndSubmit(TheTestUser);
+            Assert.IsTrue(ultimateQAHomePage.IsVisible, "UltimateQA home page was not visible");
+        }
+
        
 
         [TestMethod]
-        public void Test1()
+        [Description("Fake 2nd test.")]
+        public void Test2()
         {
+            SampleAppPage.GoTo();
+            //Assert.IsTrue(SampleAppPage.IsVisible, "Sample application page was not visible");
+            SampleAppPage.FilloutEmergencyContact(EmergencyContactUser);
+            var ultimateQAHomePage = SampleAppPage.FillOutPrimaryContactFormAndSubmit(TheTestUser);
+            AssertPageVisibleVariation2(ultimateQAHomePage);
+        }       
+
+        [TestMethod]
+        [Description("Validate that when selecting the Other gender type, the form is submitted successfully.")]
+        public void Test3()
+        {
+            SetGenderTypes(Gender.Other, Gender.Other);
+            SampleAppPage.GoTo();
+            SampleAppPage.FilloutEmergencyContact(EmergencyContactUser);
+            var ultimateQAHomePage = SampleAppPage.FillOutPrimaryContactFormAndSubmit(TheTestUser);
+            AssertPageVisibleVariation2(ultimateQAHomePage);
+        }
+
+        [TestInitialize]
+        public void SetupForEverySingleTestMethod()
+        {
+            SampleAppPage = new SampleApplicationPage(Driver);
             Driver = GetChromeDriver();
-            var sampleApplicationPage = new SampleApplicationPage(Driver);
-            sampleApplicationPage.GoTo();
-            Assert.IsTrue(sampleApplicationPage.IsVisible, "Sample application page was not visible");
+            TheTestUser = new TestUser();
+            TheTestUser.FirstName = "Pedro";
+            TheTestUser.LastName = "Porte";
 
-            var ultimateQAHomePage = sampleApplicationPage.FillOutFormAndSubmit("Pedro");
-            Assert.IsTrue(ultimateQAHomePage.IsVisible, "UltimateQA home page was not visible");
+            EmergencyContactUser = new TestUser();
+            EmergencyContactUser.FirstName = "Angela";
+            EmergencyContactUser.LastName = "Fin";
 
+
+        }
+
+        [TestCleanup]
+        public void CleanUpAfterEveryTestMethod()
+        {
             Driver.Quit();
-
         }
 
         private IWebDriver GetChromeDriver()
@@ -34,5 +81,23 @@ namespace SampleFramework1
             var outputDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             return new ChromeDriver(outputDirectory);
         }
+
+        private static void AssertPageVisible(UltimateQAHomePage ultimateQAHomePage)
+        {
+            Assert.IsTrue(ultimateQAHomePage.IsVisible, "UltimateQA home page was not visible");
+        }
+
+        private static void AssertPageVisibleVariation2(UltimateQAHomePage ultimateQAHomePage)
+        {
+            Assert.IsFalse(!ultimateQAHomePage.IsVisible, "UltimateQA home page was not visible");
+        }
+
+        private void SetGenderTypes(Gender primaryContact, Gender emergencyContact)
+        {
+            TheTestUser.GenderType = primaryContact;
+            EmergencyContactUser.GenderType = emergencyContact;
+        }
+
+       
     }
 }
--- SampleFramework1/SampleApplicationPage.cs
+++ SampleFramework1/SampleApplicationPage.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 
 namespace SampleFramework1
 {
     internal class SampleApplicationPage : BaseSampleApplicationPage
     {
-
+       
 
         public SampleApplicationPage(IWebDriver driver) : base(driver){}
 
@@ -13,27 +14,96 @@ namespace SampleFramework1
 
         public bool IsVisible
         {
-            get { return Driver.Title.Contains("Sample Application Lifecycle - Sprint 1 - Ultimate QA"); }
+            get { return Driver.Title.Contains(PageTitle); }
 
             set { }
         }
 
+        public string PageTitle => "Sample Application Lifecycle - Sprint 4 - Ultimate QA";
+
         public IWebElement FirstNameField => Driver.FindElement(By.Name("firstname"));
 
-        public IWebElement SubmitButton => Driver.FindElement(By.Id("submitForm"));
+        public IWebElement SubmitButton => Driver.FindElement(By.XPath("//*[@type='submit']"));
 
-        
+        public IWebElement LastNameField => Driver.FindElement(By.Name("lastname"));
 
-        internal void GoTo()
+        public IWebElement MaleRadioButton => Driver.FindElement(By.XPath("//*[@value='male']"));
+
+        public IWebElement FemaleRadioButton => Driver.FindElement(By.XPath("//*[@value='female']"));
+
+        public IWebElement OtherRadioButton => Driver.FindElement(By.XPath("//*[@value='other']"));
+
+        public IWebElement MaleEmergencyRadioButton => Driver.FindElement(By.Id("radio2-m"));
+
+        public IWebElement FemaleEmergencyRadioButton => Driver.FindElement(By.Id("radio2-f"));
+
+        public IWebElement OtherEmergencyRadioButton => Driver.FindElement(By.Id("radio2-o"));
+
+        public IWebElement FirstNameEmergencyField => Driver.FindElement(By.Id("f2"));
+
+        public IWebElement LastNameEmergencyField => Driver.FindElement(By.Id("l2"));
+
+        public void GoTo()
         {
-            Driver.Navigate().GoToUrl("https://ultimateqa.com/sample-application-lifecycle-sprint-1/");
+            Driver.Navigate().GoToUrl("https://www.ultimateqa.com/sample-application-lifecycle-sprint-4/");
+            Assert.IsTrue(IsVisible,
+                $"Sample application page was not visible. Expected=>{PageTitle}." +
+                $"Actual=>{Driver.Title}");
         }
 
-        internal UltimateQAHomePage FillOutFormAndSubmit(string firstName)
+        internal UltimateQAHomePage FillOutPrimaryContactFormAndSubmit(TestUser user)
         {
-            FirstNameField.SendKeys(firstName); ;
+            SetGender(user);
+            FirstNameField.SendKeys(user.FirstName);
+            LastNameField.SendKeys(user.LastName);
             SubmitButton.Click();
             return new UltimateQAHomePage(Driver);
         }
+
+        internal void FilloutEmergencyContact(TestUser emergencyContact)
+        {
+            SetEmergencyGender(emergencyContact);
+            FirstNameEmergencyField.SendKeys(emergencyContact.FirstName);
+            LastNameEmergencyField.SendKeys(emergencyContact.LastName);
+            
+        }
+
+        private void SetGender(TestUser user)
+        {
+            switch (user.GenderType)
+            {
+                case Gender.Male:
+                    MaleRadioButton.Click();
+                    break;
+                case Gender.Female:
+                    FemaleRadioButton.Click();
+                    break;
+                case Gender.Other:
+                    OtherRadioButton.Click();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void SetEmergencyGender(TestUser emergencyContact)
+        {
+            switch (emergencyContact.GenderType)
+            {
+                case Gender.Male:
+                    MaleEmergencyRadioButton.Click();
+                    break;
+                case Gender.Female:
+                    FemaleEmergencyRadioButton.Click();
+                    break;
+                case Gender.Other:
+                    OtherEmergencyRadioButton.Click();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+       
     }
 }
